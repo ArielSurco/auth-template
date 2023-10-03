f086226ee320a17878ea6e81c2d074de31990134
ArielSurco <asurco@midasconsultores.com.ar>
2023-10-03 13:01:08 -0300

    Fix break when credentials are wrong

--- src/auth/controllers/sign-in.ts
+++ src/auth/controllers/sign-in.ts
@@ -1,13 +1,17 @@
-import type { Request, Response } from 'express';
+import type { NextFunction, Request, Response } from 'express';
 
 import { userRepository } from '../dependencies';
 import { authUser } from '../modules/application/auth-user';
 
-export const signIn = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
-
-  const token = await authUser(userRepository, { email, password });
-
-  res.cookie('token', token, { httpOnly: true });
-  res.status(200).json({ message: 'User authenticated' });
+export const signIn = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+  
+    const token = await authUser(userRepository, { email, password });
+  
+    res.cookie('token', token, { httpOnly: true });
+    res.status(200).json({ message: 'User authenticated' });
+  } catch (err) {
+    next(err);
+  }
 };
